5dda71b123bd01aa35acfea20deaa7782de2f336
Ricky428 <103875348+Ricky428@users.noreply.github.com>
2022-06-29 17:58:51 +0530

    Update fetchPosts.js

--- src/api/fetchPosts.js
+++ src/api/fetchPosts.js
@@ -1,8 +1,9 @@
 import axios from "axios"
 const fetchPosts = async (page, limit) => {
     // optionally write fetching logic here or somewhere else
-   const res = await axios.get(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=5`)
-    return res
+   const res = await axios.get(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=5`);
+   console.log(res);
+    return res;
 }
 
 export { fetchPosts }
