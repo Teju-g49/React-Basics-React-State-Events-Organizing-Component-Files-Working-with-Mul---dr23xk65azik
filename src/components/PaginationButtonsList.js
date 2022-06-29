ed51a45e5e7302d0baec388901988e77adee9034
Ricky428 <103875348+Ricky428@users.noreply.github.com>
2022-06-29 19:17:18 +0530

    Update PaginationButtonsList.js

--- src/components/PaginationButtonsList.js
+++ src/components/PaginationButtonsList.js
@@ -1,9 +1,16 @@
 import React from 'react'
 const PaginationButtonsList = () => {
-
+    const noOfPages = 20            /*to calculate pages => (posts/limit) like here 100/5*/
+    function createButtons () {
+        let buttonArr = [];
+        for(let i=0; i<noOfPages; i++) {
+            buttonArr.push(<button>{i+1}</button>)          /*we are creating button here we can use createElement as well*/
+        }
+        return buttonArr;
+    }
     return (
         <div className="pagination-buttons-list">
-
+        {createButtons()}
         </div>
     )
 }
