15adf640825ec4f3527ab2547dfaf8820836213d
Ricky428 <103875348+Ricky428@users.noreply.github.com>
2022-06-29 18:31:37 +0530

    Update PostList.js

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
+import{fetchPosts} from '../api/fetchPosts'
 
 
 const PostList = () => {
-    
+    const[page,setPage] = useState(1);
+    const[posts,setPosts] = useState([])
+    const limit = 5;
+    useEffect(()=> {
+        fetchPosts(page,limit).then(()=>{
+            setPost([...res.data])
+        })
+    },[page])
     return (
         <>
-
+        post.map(()=>{)
         </>
     )
 }
